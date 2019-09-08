2ea05dfa6a2b604707984e2764c8ee27a3d357bb
Alexander Granin <graninas@gmail.com>
2019-09-09 03:22:19 +0700

    Sample API (Servant-based) added.

--- app/credit_platform/Services/API/Types.hs
+++ app/credit_platform/Services/API/Types.hs
@@ -3,3 +3,46 @@ module Services.API.Types where
 import EulerHS.Prelude
 import EulerHS.Language
 import EulerHS.Types
+
+
+
+
+type SampleAPI
+         = "position"  :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
+      :<|> "hello"     :> QueryParam "name" String :> Get '[JSON] HelloMessage
+      :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
+
+sampleAPI :: Proxy SampleAPI
+sampleAPI = Proxy
+
+position :<|> hello :<|> marketing = client sampleAPI
+
+data Position = Position
+  { xCoord :: Int
+  , yCoord :: Int
+  } deriving (Show, Generic)
+
+instance FromJSON Position
+
+newtype HelloMessage = HelloMessage { msg :: String }
+  deriving (Show, Generic)
+
+instance FromJSON HelloMessage
+
+data ClientInfo = ClientInfo
+  { clientName :: String
+  , clientEmail :: String
+  , clientAge :: Int
+  , clientInterestedIn :: [String]
+  } deriving Generic
+
+instance ToJSON ClientInfo
+
+data Email = Email
+  { from :: String
+  , to :: String
+  , subject :: String
+  , body :: String
+  } deriving (Show, Generic)
+
+instance FromJSON Email

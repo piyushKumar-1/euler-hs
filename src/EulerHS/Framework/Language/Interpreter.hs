a482eb6af4835da70913bc10db17e204ded004f0
Vasily Gorev <v@gorev.one>
2019-09-11 16:02:41 +0300

    redundant interpreter removed

--- src/EulerHS/Framework/Language/Interpreter.hs
+++ src/EulerHS/Framework/Language/Interpreter.hs
@@ -2,7 +2,6 @@ module EulerHS.Framework.Language.Interpreter where
 
 import           EulerHS.Prelude
 import           EulerHS.Framework.Language.Flow
-import           EulerHS.Framework.Language.Flow as L
 import           EulerHS.Framework.Language.Types
 import           Data.Aeson                      (encode, decode)
 import           Network.HTTP.Client             (Manager)
@@ -35,14 +34,5 @@ interpretFlowMethod Runtime{..} (SetOption k v next) =
       let newMap = Map.insert (BSL.toStrict $ encode k) (BSL.toStrict $ encode v) m
       putMVar options newMap
 
-interpretFlowMethod Runtime{..} (CallServantAPI bUrl clientAct continuation) = do
-  continuation <$> catchAny (S.runClientM clientAct (S.mkClientEnv networkManager bUrl)) (pure . Left . S.ConnectionError)
-
-
-interpretFlowMethodL :: MVar Manager -> L.FlowMethod a -> IO a
-interpretFlowMethodL m (L.CallServantAPI bUrl clientAct continuation) = do
-  man <- takeMVar m
-  continuation <$> catchAny (S.runClientM clientAct (S.mkClientEnv man bUrl)) (pure . Left . S.ConnectionError)
-
-runFlowMethodL :: MVar Manager -> L.Flow a -> IO a
-runFlowMethodL m = foldF (interpretFlowMethodL m)
+interpretFlowMethod Runtime{..} (CallServantAPI bUrl clientAct next) = do
+  next <$> catchAny (S.runClientM clientAct (S.mkClientEnv networkManager bUrl)) (pure . Left . S.ConnectionError)

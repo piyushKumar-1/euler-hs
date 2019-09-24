99c28114d68ec3397db686fc5aa22480e6f6b447
Abhik Khanra <abhik@juspay.in>
2019-09-24 12:13:45 +0530

    dashboard: Move JSON instances to end of the file
    Makes types easier to read.

--- lib/dashboard/src/Dashboard/Query/Types.hs
+++ lib/dashboard/src/Dashboard/Query/Types.hs
@@ -18,9 +18,6 @@ data SelectField
   | Field FieldName
   deriving (Generic, Show, Eq)
 
-instance FromJSON SelectField
-instance ToJSON SelectField
-
 type TableName = String
 
 data SelectOp
@@ -29,36 +26,26 @@ data SelectOp
   | AVG
   deriving (Generic, Show, Eq)
 
-instance FromJSON SelectOp
-instance ToJSON SelectOp
-
 -- FIXME: Extend to multiple fields for custom queries
-newtype Selection = Selection (SelectOp, SelectField)
+newtype Selection =
+  Selection (SelectOp, SelectField)
   deriving (Generic, Show, Eq)
 
-instance FromJSON Selection
-instance ToJSON Selection
-
-newtype Timestamp = Timestamp UTCTime
+newtype Timestamp =
+  Timestamp UTCTime
   deriving (Generic, Show, Eq)
 
-instance FromJSON Timestamp
-instance ToJSON Timestamp
-
-newtype Milliseconds = Milliseconds Int64
+newtype Milliseconds =
+  Milliseconds Int64
   deriving (Generic, Show, Eq)
 
-instance FromJSON Milliseconds
-instance ToJSON Milliseconds
-
-data Interval = Interval { start :: Timestamp
-                         , stop  :: Timestamp
-                         , step  :: Maybe Milliseconds
-                         }
+data Interval =
+  Interval
+    { start :: Timestamp
+    , stop :: Timestamp
+    , step :: Maybe Milliseconds
+    }
   deriving (Generic, Show, Eq)
-
-instance FromJSON Interval
-instance ToJSON Interval
 
 -- Needs to be any supported data type, not just String
 data Value
@@ -67,72 +54,117 @@ data Value
   | FloatValue Float
   deriving (Generic, Show, Eq)
 
-instance FromJSON Value
-instance ToJSON Value
-
 data FilterOp =
   EQUALS
   deriving (Generic, Show, Eq)
 
-instance FromJSON FilterOp
-instance ToJSON FilterOp
-
-newtype Filter = Filter [(FieldName, FilterOp, Value)]
+newtype Filter =
+  Filter [(FieldName, FilterOp, Value)]
   deriving (Generic, Show, Eq)
 
-instance FromJSON Filter
-instance ToJSON Filter
-
-newtype GroupBy = GroupBy [FieldName]
+newtype GroupBy =
+  GroupBy [FieldName]
   deriving (Generic, Show, Eq)
-
-instance FromJSON GroupBy
-instance ToJSON GroupBy
 
 data Query =
   Query Selection TableName Interval Filter GroupBy
   deriving (Generic, Show, Eq)
 
-instance FromJSON Query
-instance ToJSON Query
-
 data QueryResultValue =
   QueryResultValue Timestamp Timestamp Value
   deriving (Generic, Show, Eq)
-
-instance ToJSON QueryResultValue
-instance FromJSON QueryResultValue
 
 data QueryResult
   = SingleResult QueryResultValue
   | MultipleResult [(String, QueryResult)]
   deriving (Generic, Show, Eq)
 
-instance ToJSON QueryResult
-instance FromJSON QueryResult
-
 data Table =
   Table TableName [FieldName]
   deriving (Generic, Show, Eq)
 
-instance FromJSON Table
-instance ToJSON Table
-
-newtype QueryConfiguration = QueryConfiguration [Table]
+newtype QueryConfiguration =
+  QueryConfiguration [Table]
   deriving (Generic, Show)
-
-instance FromJSON QueryConfiguration
-instance ToJSON QueryConfiguration
 
 data QueryValidationError =
   QueryValidationError QueryErrorType String
   deriving (Generic, Show, Eq)
-instance FromJSON QueryValidationError
-instance ToJSON QueryValidationError
 
 --Assuming single fields
-data QueryErrorType =
-  TableFieldError TableName | SelectFieldError FieldName | FilterFieldError FieldName | GroupByFieldError FieldName | FieldError FieldName
+data QueryErrorType
+  = TableError TableName
+  | SelectFieldError FieldName
+  | FilterFieldError FieldName
+  | GroupByFieldError FieldName
+  | FieldError FieldName
   deriving (Generic, Show, Eq)
+
+-- All the JSON instances
+instance FromJSON Selection
+
+instance ToJSON Selection
+
+instance FromJSON Timestamp
+
+instance ToJSON Timestamp
+
+instance FromJSON Milliseconds
+
+instance ToJSON Milliseconds
+
+instance FromJSON Interval
+
+instance ToJSON Interval
+
+instance FromJSON Value
+
+instance ToJSON Value
+
+instance FromJSON FilterOp
+
+instance ToJSON FilterOp
+
+instance FromJSON Filter
+
+instance ToJSON Filter
+
+instance FromJSON GroupBy
+
+instance ToJSON GroupBy
+
+instance FromJSON Query
+
+instance ToJSON Query
+
+instance ToJSON QueryResultValue
+
+instance FromJSON QueryResultValue
+
+instance ToJSON QueryResult
+
+instance FromJSON QueryResult
+
+instance FromJSON Table
+
+instance ToJSON Table
+
+instance FromJSON QueryConfiguration
+
+instance ToJSON QueryConfiguration
+
+instance FromJSON SelectField
+
+instance ToJSON SelectField
+
+instance FromJSON SelectOp
+
+instance ToJSON SelectOp
+
 instance FromJSON QueryErrorType
+
 instance ToJSON QueryErrorType
+
+instance FromJSON QueryValidationError
+
+instance ToJSON QueryValidationError

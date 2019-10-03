61ef8550692377ad4c22897c70166fa7259068be
Kaptch <kaptch@gmail.com>
2019-10-03 10:59:43 +0200

    merge conflict resolve

--- lib/euler-hs/test/EulerHS/Testing/Flow/Runtime.hs
+++ lib/euler-hs/test/EulerHS/Testing/Flow/Runtime.hs
@@ -4,3 +4,15 @@ import           EulerHS.Prelude
 import           EulerHS.Runtime
 
 import           Network.HTTP.Client     (defaultManagerSettings, newManager)
+import           Database.Redis (checkedConnect, defaultConnectInfo, Redis(..))
+import           Data.Map (singleton)
+
+type FlowRtInitializer = IO FlowRuntime
+
+initDefaultFlowRt :: FlowRtInitializer
+initDefaultFlowRt = do
+  manager <- newMVar =<< newManager defaultManagerSettings
+  options <- newMVar mempty
+  lrt <- createVoidLoggerRuntime
+  conn <- newEmptyMVar
+  pure $ FlowRuntime lrt manager options conn

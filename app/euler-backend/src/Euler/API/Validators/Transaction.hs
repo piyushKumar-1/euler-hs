da518814a300810775daac49725fc33424bbef18
Kirill Gavrilyuk <k.gavrilyuk@atomcream.com>
2019-10-31 14:33:48 +0200

    Validation Refactoring / Cleanups

--- app/euler-backend/src/Euler/API/Validators/Transaction.hs
+++ app/euler-backend/src/Euler/API/Validators/Transaction.hs
@@ -3,7 +3,7 @@
 module Euler.API.Validators.Transaction where
 
 import EulerHS.Prelude
-import EulerHS.Extra.Validation
+import EulerHS.Extra.Validation as V
 
 import Data.Generics.Product.Fields
 import Data.Functor.Alt
@@ -23,120 +23,114 @@ import Euler.Product.Domain.PaymentMethod.UPI
 import qualified Euler.API.Transaction as AT
 import qualified Euler.API.Types as AT
 import Euler.Common.Transaction (AuthType(..))
+import Euler.API.Types
 
+
+-- TODO Probably we can factor explicit apiTxn out with the help of ReaderT
 instance Transform AT.Transaction DT.Transaction where
   transform apiTxn = DT.Transaction
-      <$> (OrderId <$> ((takeField @"order_id" apiTxn) <!*> textNotEmpty)) -- :: OrderId                -- ^
-      <*> (MerchantId <$> ((takeField @"merchant_id" apiTxn) <!*> textNotEmpty)) -- :: MerchantId
-      <*> (transform apiTxn) -- :: TransactionType
-      <*> (takeField @"redirect_after_payment" apiTxn) <!*> alwaysValid -- :: Bool
-      <*> (takeField @"format" apiTxn) <!*> textNotEmpty -- :: Text
-
+      <$> (OrderId    <$> withField @"order_id"    apiTxn textNotEmpty) -- :: OrderId                -- ^
+      <*> (MerchantId <$> withField @"merchant_id" apiTxn textNotEmpty) -- :: MerchantId
+      <*> transform apiTxn                                               -- :: TransactionType
+      <*> withField @"redirect_after_payment" apiTxn pure                -- :: Bool
+      <*> withField @"format" apiTxn textNotEmpty                       -- :: _
 
 instance Transform AT.Transaction DT.TransactionType where
   transform apiTxn = case ( getField @"payment_method_type" apiTxn
                           , getField @"auth_type" apiTxn
                           , getField @"direct_wallet_token" apiTxn) of
-    (AT.CARD, Just ATMPIN, _) -> DT.ATMRedirectionTransaction <$> transform apiTxn
-                            <!>  DT.ATMSeamlessTransaction <$> transform apiTxn
-    (AT.CARD,   _        , _) -> DT.CardTransaction <$> transform apiTxn
-    (AT.NB  ,   _        , _) -> DT.NBTransaction <$> transform apiTxn
-    (AT.WALLET, _  , Nothing) -> DT.WalletTransaction <$> transform apiTxn
-    (AT.WALLET, _  ,  Just _) -> DT.WalletDirectDebitTransaction <$> transform apiTxn
-    (AT.UPI,    _        , _) -> DT.UPITransaction <$> transform apiTxn
+    (AT.CARD  , Just ATMPIN, _      ) -> DT.ATMRedirectionTransaction    <$> transform apiTxn
+                                     <!> DT.ATMSeamlessTransaction       <$> transform apiTxn
+    (AT.CARD  , _          , _      ) -> DT.CardTransaction              <$> transform apiTxn
+    (AT.NB    , _          , _      ) -> DT.NBTransaction                <$> transform apiTxn
+    (AT.WALLET, _          , Nothing) -> DT.WalletTransaction            <$> transform apiTxn
+    (AT.WALLET, _          , Just _ ) -> DT.WalletDirectDebitTransaction <$> transform apiTxn
+    (AT.UPI   , _          , _      ) -> DT.UPITransaction               <$> transform apiTxn
 
 instance Transform AT.Transaction ATMSeamlessPayment where
   transform apiTxn = ATMSeamlessPayment
-    <$> (decodeTo @CardPaymentMethod $ fromMaybe' @"payment_method" apiTxn)
-        <!*> alwaysValid  -- payment_method :: CPM.CardPaymentMethod
-    <*> (transform apiTxn) -- card_payment_type      :: CardPaymentType
-    <*> (fromMaybe' @"auth_type" apiTxn) <!*> isAtmCardAuthType
+    <$> withField @"payment_method" apiTxn (extractJust >=> decode)
+    <*> transform apiTxn
+    <*> withField @"auth_type"      apiTxn (extractJust >=> isAtmCardAuthType)
 
 instance Transform AT.Transaction ATMRedirectionPayment where
   transform apiTxn = ATMRedirectionPayment
-    <$> (decodeTo @ATMCardPaymentMethod $ fromMaybe' @"payment_method" apiTxn)
-        <!*> alwaysValid
-    <*> (fromMaybe' @"auth_type" apiTxn) <!*> isAtmCardAuthType
+    <$> withField @"payment_method" apiTxn (extractJust >=> decode)
+    <*> withField @"auth_type"      apiTxn (extractJust >=> isAtmCardAuthType)
 
 instance Transform AT.Transaction UPIPayment where
   transform apiTxn
     =   UPICollect
-        <$> (decodeTo @UPIPaymentMethod $ fromMaybe' @"payment_method" apiTxn)
-            <!*> alwaysValid
-        <*> (decodeTo @UPITxnType $ fromMaybe' @"txn_type" apiTxn)
-            <!*> isUPICollectTxnType
-        <*> (fromMaybe' @"upi_vpa" apiTxn) <!*> textNotEmpty
+        <$> withField @"payment_method" apiTxn (extractJust >=> decode)
+        <*> withField @"txn_type"       apiTxn (extractJust >=> decode >=> isUPICollectTxnType)
+        <*> withField @"upi_vpa"        apiTxn (extractJust >=> textNotEmpty)
     <!> UPIPay
-        <$> (decodeTo @UPIPaymentMethod $ fromMaybe' @"payment_method" apiTxn)
-            <!*> alwaysValid
-        <*> (decodeTo @UPITxnType $ fromMaybe' @"txn_type" apiTxn)
-            <!*> isUPIPayTxnType
+        <$> withField @"payment_method" apiTxn (extractJust >=> decode)
+        <*> withField @"txn_type"       apiTxn (extractJust >=> decode >=> isUPIPayTxnType)
 
 instance Transform AT.Transaction WalletPayment where
   transform apiTxn = WalletPayment
-    <$> (decodeTo @WalletPaymentMethod $ fromMaybe' @"payment_method" apiTxn)
-        <!*> alwaysValid
+    <$> withField @"payment_method" apiTxn (extractJust >=> decode)
 
 instance Transform AT.Transaction DirectWalletPayment where
   transform apiTxn = DirectWalletPayment
-    <$> (decodeTo @WalletDirectPaymentMethod $ fromMaybe' @"payment_method" apiTxn)
-        <!*> alwaysValid
-    <*> (fromMaybe' @"direct_wallet_token" apiTxn) <!*> textNotEmpty
+    <$> withField @"payment_method"      apiTxn (extractJust >=> decode)
+    <*> withField @"direct_wallet_token" apiTxn (extractJust >=> textNotEmpty)
 
 instance Transform AT.Transaction NBPayment where
   transform apiTxn = NBPayment
-    <$> ( decodeTo @NBPaymentMethod $ fromMaybe' @"payment_method" apiTxn)
-        <!*> alwaysValid
+    <$> withField @"payment_method" apiTxn (extractJust >=> decode)
 
 instance Transform AT.Transaction CardPayment where
   transform apiTxn = CardPayment
-    <$> (decodeTo @CardPaymentMethod $ fromMaybe' @"payment_method" apiTxn)
-        <!*> alwaysValid  -- payment_method :: CPM.CardPaymentMethod
-    <*> (transform apiTxn) -- card_payment_type      :: CardPaymentType
-    <*> (takeField @"is_emi" apiTxn ) <?*> alwaysValid
-    <*> (takeField @"emi_bank" apiTxn) <?*> textNotEmpty
-    <*> (takeField @"emi_tenure" apiTxn) <?*> alwaysValid
-    <*> (takeField @"auth_type" apiTxn) <?*> isRegularCardAuthType
+    <$> withField @"payment_method" apiTxn (extractJust >=> decode)
+    <*> transform apiTxn
+    <*> withField @"is_emi"     apiTxn pure
+    <*> withField @"emi_bank"   apiTxn (insideJust textNotEmpty)
+    <*> withField @"emi_tenure" apiTxn pure
+    <*> withField @"auth_type"  apiTxn (insideJust isRegularCardAuthType)
 
 instance Transform AT.Transaction CardPaymentType where
   transform apiTxn
     =   SavedCardPayment
-          <$> (fromMaybe' @"card_token" apiTxn) <!*> textNotEmpty
-          <*> (fromMaybe' @"card_security_code" apiTxn) <!*> textNotEmpty
+          <$> withField @"card_token"         apiTxn (extractJust >=> textNotEmpty)
+          <*> withField @"card_security_code" apiTxn (extractJust >=> textNotEmpty)
     <!> NewCardPayment
-          <$> (fromMaybe' @"card_number" apiTxn) <!*> cardNumberValidators
-          <*> (fromMaybe' @"name_on_card" apiTxn ) <!*> textNotEmpty
-          <*> (fromMaybe' @"card_exp_month" apiTxn ) <!*> textNotEmpty
-          <*> (fromMaybe' @"card_exp_year" apiTxn ) <!*> textNotEmpty
-          <*> (fromMaybe' @"card_security_code" apiTxn ) <!*> textNotEmpty
-          <*> (fromMaybe' @"save_to_locker" apiTxn ) <!*> alwaysValid
+          <$> withField @"card_number"        apiTxn (extractJust >=> cardNumberValidators)
+          <*> withField @"name_on_card"       apiTxn (extractJust >=> textNotEmpty)
+          <*> withField @"card_exp_month"     apiTxn (extractJust >=> textNotEmpty)
+          <*> withField @"card_exp_year"      apiTxn (extractJust >=> textNotEmpty)
+          <*> withField @"card_security_code" apiTxn (extractJust >=> textNotEmpty)
+          <*> withField @"save_to_locker"     apiTxn extractJust
 
 
-isRegularCardAuthType :: NonEmpty (Text -> AuthType -> Validation [Text] AuthType)
-isRegularCardAuthType = mkValidator "inappropriate auth_type" (`elem` [THREE_DS, OTP, VISA_CHECKOUT])
+----------------------------------------------------------------------
 
-isAtmCardAuthType:: NonEmpty (Text -> AuthType -> Validation [Text] AuthType)
-isAtmCardAuthType = mkValidator "inappropriate auth_type" (== ATMPIN)
+isRegularCardAuthType :: Validator AuthType
+isRegularCardAuthType = mkValidator "inappropriate field type" (`elem` [THREE_DS, OTP, VISA_CHECKOUT])
 
-cardNumberValidators :: NonEmpty (Text -> Text -> Validation [Text] Text)
-cardNumberValidators = textNotEmpty <> testValidator1 <> testValidator2
+isAtmCardAuthType :: Validator AuthType
+isAtmCardAuthType = mkValidator "inappropriate field type" (== ATMPIN)
 
-alwaysValid :: NonEmpty (Text -> t -> Validation [Text] t)
-alwaysValid = mkValidator "always should pass" (const True)
-
-textNotEmpty :: NonEmpty (Text -> Text -> Validation [Text] Text)
+textNotEmpty :: Validator Text
 textNotEmpty = mkValidator "can't be empty" (not . T.null)
 
-testValidator1 :: NonEmpty (Text -> b -> Validation [Text] b)
+isUPICollectTxnType :: Validator UPITxnType
+isUPICollectTxnType = mkValidator "inappropriate txn_type" (== UPI.UPI_COLLECT)
+
+isUPIPayTxnType :: Validator UPITxnType
+isUPIPayTxnType = mkValidator "inappropriate txn_type" (`elem` [UPI.UPI_PAY, UPI.BHARAT_PAY])
+
+cardNumberValidators :: Validator Text
+cardNumberValidators =
+  parValidate
+    [ textNotEmpty
+    , testValidator1
+    , testValidator2
+    ]
+
+testValidator1 :: Validator a
 testValidator1  = mkValidator "always should pass" (const True)
 
-testValidator2 :: NonEmpty (Text -> b -> Validation [Text] b)
-testValidator2  = mkValidator "always should pass" (const True)
-
-isUPICollectTxnType :: NonEmpty (Text -> UPITxnType -> Validation [Text] UPITxnType)
-isUPICollectTxnType = mkValidator "inappropriate txn_type" (`elem` txnTypes)
-  where txnTypes = [UPI.UPI_COLLECT]
-
-isUPIPayTxnType :: NonEmpty (Text -> UPITxnType -> Validation [Text] UPITxnType)
-isUPIPayTxnType = mkValidator "inappropriate txn_type" (`elem` txnTypes)
-  where txnTypes = [UPI.UPI_PAY, UPI.BHARAT_PAY]
+testValidator2 :: Validator a
+testValidator2  = pure
--- lib/euler-hs/src/EulerHS/Extra/Validation.hs
+++ lib/euler-hs/src/EulerHS/Extra/Validation.hs
@@ -1,20 +1,21 @@
 {-# OPTIONS -fno-warn-deprecations #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+
 module EulerHS.Extra.Validation
   ( Transform(..)
   , mkValidator
-  , decodeTo
-  , fromMaybe'
-  , takeField
-  , (<?*>)
-  , (<!*>)
-  , (<<|>>)
-  , showConstructors
-  , getConstructors
+  , Transformer
+  , Validator
+  , Errors
   , module X
+  , withField
+  , extractJust
+  , decode
+  , insideJust
+  , parValidate
   ) where
 
-import EulerHS.Prelude hiding ( pred, or)
+import EulerHS.Prelude hiding (pred, or)
 import qualified Prelude as P
 
 import Data.Generics.Product.Fields
@@ -26,103 +27,57 @@ import Type.Reflection
 import Control.Lens hiding (transform, cons)
 import qualified Data.Text as T
 
+type Ctx = Text
+type Errors = [Text]
+
+-- TODO: Looks like Profunctor. Does it hold laws?
+type Transformer a b = a -> ReaderT Ctx (Either Errors) b
+
+type Validator a = Transformer a a
 
 class Transform a b where
   transform :: a -> Validation [Text] b
 
 -- takes error message and predicate and return validation function
-mkValidator :: Text -> (t -> Bool) -> NonEmpty (Text -> t -> Validation [Text] t)
-mkValidator err pred = (\fn v -> if pred v then _Success # v else _Failure # [fn <> " " <> err]) :| []
+mkValidator :: Text -> (t -> Bool) -> Validator t
+mkValidator err pred v = ReaderT (\ctx -> if not $ pred v
+  then Left [ctx <> " " <> err]
+  else pure v)
 
 -- trying to decode Text to target type
-decodeTo :: forall t. (Data t, Read t) => (Text, Validation [Text] Text) -> (Text, Validation [Text] t)
-decodeTo (f, Failure e) = (f, Failure e)
-decodeTo (f, Success v) = case (readMaybe $ toString v) of
-  Just x -> (f, _Success # x)
-  _ -> (f, _Failure # ["Can't decode " <> v <> " from field " <> f <> ", should be one of " <> (showConstructors @t)])
+decode :: forall t . (Data t, Read t) => Transformer Text t
+decode v = ReaderT (\ctx -> case (readMaybe $ toString v) of
+  Just x -> Right x
+  _      -> Left ["Can't decode " <> v <> " from field " <> ctx <> ", should be one of " <> showConstructors @t])
+
+insideJust :: Transformer a b -> Transformer (Maybe a) (Maybe b)
+insideJust val Nothing  = pure Nothing
+insideJust val (Just a) = Just <$> val a
 
 -- trying to extract the argument from Maybe type
 -- if value is Nothing then raise Failure
-fromMaybe' :: forall (f :: Symbol) t r
-  . ( HasField' f r (Maybe t), KnownSymbol f)
-  => r -> (Text, Validation [Text] t)
-fromMaybe' r = ((fieldName_ @f) , isPresent' (fieldName_ @f) $ getField @f r)
+extractJust :: Transformer (Maybe a) a
+extractJust r = ReaderT (\ctx -> maybe (Left [ctx <> " not present"]) Right r)
 
-isPresent' :: Text -> Maybe t -> Validation [Text] t
-isPresent' f v = maybe (_Failure # [f <> " not present"]) (_Success # ) v
-
---extract value and put it in 'Success'
-takeField :: forall (f :: Symbol) v r
-  .(Generic r, HasField' f r v, KnownSymbol f)
-  =>  r -> (Text, Validation [Text] v)
-takeField r = (fieldName_ @f, Success $ getField @f r)
-
--- applies validators to the extracted value
--- for optional fields
-ifPresent :: (Text, Validation [Text] (Maybe v))
-  -> NonEmpty((Text -> v -> Validation [Text] v)) --(Text -> v -> Validation [Text] v)
-  -> Validation [Text] (Maybe v)
-ifPresent (fName, Success (Just v)) vs = Just <$> (appValidators vs fName v)
-ifPresent (_, Success Nothing) _ = Success Nothing
-ifPresent (_, Failure e) _ = Failure e
-
--- applies validators to the extracted value
--- for optional fields
-infix 5 <?*>
-(<?*>) :: (Text, Validation [Text] (Maybe v))
-  -> NonEmpty((Text -> v -> Validation [Text] v)) --(Text -> v -> Validation [Text] v)
-  -> Validation [Text] (Maybe v)
-(<?*>) = ifPresent
-
--- applies validators to the extracted value
--- for mandatory fields
-ifTransformed :: (Text, Validation [Text] v)
-  -> NonEmpty((Text -> v -> Validation [Text] v)) --(Text -> v -> Validation [Text] v)
-  -> Validation [Text] v
-ifTransformed (_, Failure e) _ = Failure e
-ifTransformed (fName, Success a) vs = appValidators vs fName a
-
--- applies validators to the extracted value
--- for mandatory fields
-infix 5 <!*>
-(<!*>) :: (Text, Validation [Text] v)
-  -> NonEmpty((Text -> v -> Validation [Text] v)) --(Text -> v -> Validation [Text] v)
-  -> Validation [Text] v
-(<!*>) = ifTransformed
-
-appValidators ::  NonEmpty((Text -> v -> Validation [Text] v)) -> Text -> v -> Validation [Text] v
-appValidators validators fName v = foldr1 (<*) $ map (($ (fName, v)).uncurry) validators
+-- extract value and run validators on it
+withField
+  :: forall (f :: Symbol) v r a
+   . (Generic r, HasField' f r v, KnownSymbol f)
+  => r -> Transformer v a -> Validation Errors a
+withField rec pav = fromEither $ runReaderT (pav $ getField @f rec) $ fieldName_ @f
 
 -- return text representation of constructors of a given type
-showConstructors :: forall t. (Data t) => Text
+showConstructors :: forall t . Data t => Text
 showConstructors = T.pack $ show $ getConstructors @t
 
 -- return list with constructors of a given type
-getConstructors :: forall t. (Data t) => [Constr]
+getConstructors :: forall t . Data t => [Constr]
 getConstructors = dataTypeConstrs (dataTypeOf (undefined :: t))
 
-fieldName_ :: forall (f :: Symbol).(KnownSymbol f) => Text
+fieldName_ :: forall (f :: Symbol) . KnownSymbol f => Text
 fieldName_ = T.pack $ ((filter (/='"'))) $ P.show $ typeRep @f
 
-
--- It is used in situations when we try to create several variants at the same time
--- and there is a probability of failure of all variants at once.
--- In case of failure collects validation errors of all variants.
--- On the contrary <!> gives validation errors only from the first option
-or :: Semigroup err => Validation err a -> Validation err a -> Validation err a
-or (Failure e1) (Failure e2) =
-    Failure (e1 <> e2)
-or (Failure _) (Success a) = Success a
-or (Success a) _ = Success a
-
-infixl 3 <<|>>
-(<<|>>) :: Semigroup err => Validation err a -> Validation err a -> Validation err a
-(<<|>>) = or
-{-# INLINE (<<|>>) #-}
-
-
--- <!> (defined in Data.Validation)
--- it is used in situations when we try to create several variants at the same time
--- and there is a probability of failure of all variants at once.
--- In case of failure returns validation errors only from the first option.
--- On the contrary <<|>> collects validation errors of all variants.
+parValidate :: [Validator a] -> Validator a
+parValidate vals a = ReaderT (\ctx -> toEither $ foldr (*>) (pure a) $ fmap (mapper ctx) vals)
+  where
+    mapper ctx val = fromEither $ runReaderT (val a) ctx

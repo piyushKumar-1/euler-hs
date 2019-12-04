7c2afa5fc40c760ae4fda7996f69525f9caf6397
Alexander Granin <graninas@gmail.com>
2019-12-04 17:52:54 +0700

    Shared SQL connections WIP.

--- app/credit-platform/src/App.hs
+++ app/credit-platform/src/App.hs
@@ -2,12 +2,38 @@ module App where
 
 import EulerHS.Prelude
 
-import           Servant.Server (serve)
 import           Network.Wai.Handler.Warp (run)
+import           Data.Time.Clock (NominalDiffTime)
 
 import qualified CreditPlatform.Server as CP
 import qualified EulerHS.Runtime as R
+import qualified EulerHS.Interpreters as R
+import qualified EulerHS.Language as L
 import qualified EulerHS.Types as T
+import qualified WebService.Language as L
+import qualified WebService.Types as T
+
+
+keepConnsAliveForSecs :: NominalDiffTime
+keepConnsAliveForSecs = 60 * 10 -- 10 mins
+
+maxTotalConns :: Int
+maxTotalConns = 8
+
+sqliteConn :: IsString a => a
+sqliteConn = "sqlite"
+
+-- Test DB to show how the initialization can look like.
+-- TODO: add real DB services.
+prepareDBConnections :: L.Flow ()
+prepareDBConnections = do
+  ePool <- L.initSqlDBConnection
+    $ T.mkSQLitePoolConfig sqliteConn "tmp/test.db"
+    $ T.PoolConfig 1 keepConnsAliveForSecs maxTotalConns
+  L.throwOnFailedWithLog ePool T.SqlDBConnectionFailedException "Failed to connect to SQLite DB."
+
+prepareDBConnections' :: L.Flow ()
+prepareDBConnections' = pure ()
 
 
 runCreditPlatformApp :: IO ()
@@ -20,6 +46,10 @@ runCreditPlatformApp = do
         }
 
   R.withFlowRuntime (Just loggerCfg) $ \flowRt -> do
-    putStrLn @String "Runtime created. Starting server..."
-    let env = CP.Env flowRt
-    run port $ CP.creditPlatformApp env
+    putStrLn @String "Runtime created."
+    putStrLn @String "Initializing DB connections..."
+    try (R.runFlow flowRt prepareDBConnections') >>= \case
+      Left (e :: SomeException) -> putStrLn @String $ "Exception thrown: " <> show e
+      Right _ -> do
+        putStrLn @String "Starting web server..."
+        run port $ CP.creditPlatformApp $ CP.Env flowRt
--- app/euler-backend/src/App.hs
+++ app/euler-backend/src/App.hs
@@ -2,16 +2,42 @@ module App where
 
 import EulerHS.Prelude
 
-import           Servant.Server (serve)
-import           Network.Wai.Handler.Warp (run, Settings, setPort, defaultSettings, runSettings)
+import           Network.Wai.Handler.Warp (Settings, runSettings, setPort, defaultSettings)
+import           Data.Time.Clock (NominalDiffTime)
 
 import qualified Euler.Server as Euler
-import qualified Euler.Playback.Service as PB
 import qualified EulerHS.Runtime as R
+import qualified EulerHS.Interpreters as R
+import qualified EulerHS.Language as L
 import qualified EulerHS.Types as T
+import qualified Euler.Playback.Service as PB
+import qualified WebService.Language as L
+import qualified WebService.Types as T
+
+
+keepConnsAliveForSecs :: NominalDiffTime
+keepConnsAliveForSecs = 60 * 10 -- 10 mins
+
+maxTotalConns :: Int
+maxTotalConns = 8
+
+sqliteConn :: IsString a => a
+sqliteConn = "sqlite"
 
 eulerApiPort :: Int
 eulerApiPort = 8080
+
+-- Test DB to show how the initialization can look like.
+-- TODO: add real DB services.
+prepareDBConnections :: L.Flow ()
+prepareDBConnections = do
+  ePool <- L.initSqlDBConnection
+    $ T.mkSQLitePoolConfig sqliteConn "tmp/test.db"
+    $ T.PoolConfig 1 keepConnsAliveForSecs maxTotalConns
+  L.throwOnFailedWithLog ePool T.SqlDBConnectionFailedException "Failed to connect to SQLite DB."
+
+prepareDBConnections' :: L.Flow ()
+prepareDBConnections' = pure ()
 
 runEulerBackendApp' :: Settings -> IO ()
 runEulerBackendApp' settings = do
@@ -22,11 +48,16 @@ runEulerBackendApp' settings = do
         }
 
   R.withFlowRuntime (Just loggerCfg) $ \flowRt -> do
-    putStrLn @String "Runtime created. Starting server..."
-    recorderParams <- PB.initRecorderParams
-    let env = Euler.Env flowRt recorderParams
-    runSettings settings $ Euler.eulerBackendApp env
-
+    putStrLn @String "Runtime created."
+    putStrLn @String "Initializing DB connections..."
+    try (R.runFlow flowRt prepareDBConnections') >>= \case
+      Left (e :: SomeException) -> putStrLn @String $ "Exception thrown: " <> show e
+      Right _ -> do
+        putStrLn @String "Initializing ART..."
+        recorderParams <- PB.initRecorderParams
+        putStrLn @String "Starting web server..."
+        let env = Euler.Env flowRt recorderParams
+        runSettings settings $ Euler.eulerBackendApp env
 
 runEulerBackendApp :: IO ()
 runEulerBackendApp = runEulerBackendApp' $ setPort eulerApiPort defaultSettings

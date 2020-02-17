e8c881d1ea71f13ed5ec3d4fd01637d67daffcfb
Vasily Gorev <v@gorev.one>
2020-02-17 11:45:13 +0300

    added more tests for flow

--- lib/euler-hs/test/EulerHS/Tests/Framework/FlowSpec.hs
+++ lib/euler-hs/test/EulerHS/Tests/Framework/FlowSpec.hs
@@ -38,6 +38,10 @@ scenario1MockedValues = FlowMockedValues'
   , mockedRunSysCmd = ["Neo"]
   }
 
+ioActWithException :: IO Text
+ioActWithException = do
+  E.throw (E.AssertionFailed "Exception from IO")
+  pure "Text from IO"
 
 withServer :: IO () -> IO ()
 withServer action = do
@@ -64,7 +68,7 @@ spec = do
           res `shouldBe` (User "John" "Snow" "00000000-0000-0000-0000-000000000000")
 
       around_ withServer $ do
-        describe "CallServantAPI tests" $ do
+        describe "CallServantAPI tests with server" $ do
 
           it "Simple request (book)" $ \rt -> do
             let url = BaseUrl Http "localhost" port ""
@@ -76,22 +80,89 @@ spec = do
             userEither <- runFlow rt $ callServantAPI url getUser
             userEither `shouldSatisfy` isRight
 
+      describe "CallServantAPI tests without server" $ do
+
+        it "Simple request (book)" $ \rt -> do
+          let url = BaseUrl Http "localhost" port ""
+          bookEither <- runFlow rt $ callServantAPI url getBook
+          bookEither `shouldSatisfy` isLeft
+
+        it "Simple request (user)" $ \rt -> do
+          let url = BaseUrl Http "localhost" port ""
+          userEither <- runFlow rt $ callServantAPI url getUser
+          userEither `shouldSatisfy` isLeft
+
       it "RunIO" $ \rt -> do
         result <- runFlow rt $ runIO (pure ("hi" :: String))
         result `shouldBe` "hi"
 
-      it "Options Set Get" $ \rt -> do
-        result <- runFlow rt $ do
-          _ <- setOption @TestStringKey "lore ipsum"
-          _ <- setOption @TestStringKey2 "lore ipsum2"
-          s1 <- getOption @TestStringKey
-          s2 <- getOption @TestStringKey2
-          pure (s1,s2)
-        result `shouldBe` (Just "lore ipsum", Just "lore ipsum2")
+      it "RunIO with exception" $ \rt -> do
+        result <- E.catch
+          (runFlow rt $ do
+            runIO ioActWithException
+            pure ("Never returned" :: Text))
+          (\e -> do let err = show (e :: E.AssertionFailed)
+                    pure err)
+        result `shouldBe` ("Exception from IO" :: Text)
+
+      it "RunIO with catched exception" $ \rt -> do
+        result <-runFlow rt $ do
+            runIO $
+              E.catch
+                ioActWithException
+                (\e -> do let err = show (e :: E.AssertionFailed)
+                          pure err)
+        result `shouldBe` ("Exception from IO" :: Text)
+
+      describe "Options" $ do
+
+        it "One key" $ \rt -> do
+          result <- runFlow rt $ do
+            _ <- setOption @TestStringKey "lore ipsum"
+            getOption @TestStringKey
+          result `shouldBe` (Just "lore ipsum")
+
+        it "Not found" $ \rt -> do
+          result <- runFlow rt $ do
+            _ <- setOption @TestStringKey "lore ipsum"
+            getOption @TestStringKey2
+          result `shouldBe` Nothing
+
+
+        it "Two keys" $ \rt -> do
+          result <- runFlow rt $ do
+            _ <- setOption @TestStringKey "lore ipsum"
+            _ <- setOption @TestStringKey2 "lore ipsum2"
+            s1 <- getOption @TestStringKey
+            s2 <- getOption @TestStringKey2
+            pure (s1,s2)
+          result `shouldBe` (Just "lore ipsum", Just "lore ipsum2")
+
+--        it "Different encoding" $ \rt -> do
+--          result <- runFlow rt $ do
+--            _ <- setOption @TestStringKey "lore ipsum"
+--            _ <- setOption @TestStringKey2 "lore ipsum2"
+--            _ <- setOption @TestStringKeyAnotherEnc "lore ipsum enc"
+--            _ <- setOption @TestStringKey2AnotherEnc "lore ipsum2 enc"
+--            s1 <- getOption @TestStringKey
+--            s2 <- getOption @TestStringKey2
+--            s1enc <- getOption @TestStringKeyAnotherEnc
+--            s2enc <- getOption @TestString2KeyAnotherEnc
+--            pure (s1,s2,s1enc,s2enc)
+--          result `shouldBe` (Just "lore ipsum", Just "lore ipsum2", Just "lore ipsum enc", Just "lore ipsum2 enc")
+
 
       it "RunSysCmd" $ \rt -> do
         result <- runFlow rt $ runSysCmd "echo test"
         result `shouldBe` "test\n"
+
+      it "RunSysCmd with bad command" $ \rt -> do
+        result <- E.catch
+          (runFlow rt $ runSysCmd "badEcho test")
+          (\e -> do let err = show (e :: E.SomeException)
+                    pure err)
+        result `shouldBe` ("readCreateProcess: badEcho test (exit 127): failed" :: String)
+
 
       it "GenerateGUID" $ \rt -> do
         guid <- runFlow rt generateGUID

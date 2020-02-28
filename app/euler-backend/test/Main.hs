a0e5a8a41b2ad8b5d38cbdafcdf3a24edb310b0d
Vasily Gorev <v@gorev.one>
2020-02-28 12:29:52 +0300

    added few tests for orderUpdate and corresponding records in test.db.template

--- app/euler-backend/test/Main.hs
+++ app/euler-backend/test/Main.hs
@@ -5,6 +5,7 @@ import           EulerHS.Prelude
 import           Test.Hspec
 import qualified Euler.Tests.API.CardSpec as Card
 import qualified Euler.Tests.API.OrderSpec as Order
+import qualified Euler.Tests.API.OrderUpdateSpec as OrderUpdate
 import qualified Euler.Tests.Encryption.EncryptionSpec as Encryption
 import qualified Euler.Tests.FlexApis.SampleApiSpec as FlexApis.SampleApiSpec
 import qualified Euler.Tests.Transformation.TransactionSpec as TxnTransform
@@ -19,6 +20,7 @@ main = hspec spec
 spec :: Spec
 spec = do
     describe "Order spec"                                 Order.spec
+    describe "OrderUpdate"                                OrderUpdate.spec
     describe "Card validators spec"                       Card.spec
     describe "Txn transformation"                         TxnTransform.spec
     describe "Flex casing and content-types: sample API"  FlexApis.SampleApiSpec.spec

28c4ad0c389807c8648e377334b5e0c0a25c993c
Himanshu Prasad <himanshu.prasad@juspay.in>
2020-03-05 16:31:56 +0530

    Add multi character prefix drop option

--- lib/euler-hs/src/EulerHS/Extra/Aeson.hs
+++ lib/euler-hs/src/EulerHS/Extra/Aeson.hs
@@ -6,3 +6,11 @@ import Data.Aeson (Options, defaultOptions, fieldLabelModifier)
 
 stripLensPrefixOptions :: Options
 stripLensPrefixOptions = defaultOptions { fieldLabelModifier = drop 1 }
+
+stripAllLensPrefixOptions :: Options
+stripAllLensPrefixOptions = defaultOptions { fieldLabelModifier = dropPrefix}
+  where
+    dropPrefix :: String -> String
+    dropPrefix field = if length field > 0
+                         then dropWhile (== head field) field
+                         else field

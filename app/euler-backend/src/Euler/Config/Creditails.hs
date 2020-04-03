b5d1c8d6d672f9bc33e3d2bccc60b426e6757a0a
Vasily Gorev <v@gorev.one>
2020-04-03 17:07:58 +0300

    refactored ecMandateParamsCred & ecTempCardCred, typo in description fixed

--- app/euler-backend/src/Euler/Config/Creditails.hs
+++ app/euler-backend/src/Euler/Config/Creditails.hs
@@ -11,6 +11,8 @@ import           Euler.Config.EnvVars
 import qualified Euler.Encryption as E
 
 import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Base64 as B64
+import qualified Data.ByteString.Char8  as BC
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 
@@ -29,35 +31,39 @@ instance OptionEntity ECMandateParamsCred Text
 instance OptionEntity RazorpayClientSecret Text
 
 
-
 ecTempCardCred :: Flow ByteString
 ecTempCardCred = do
-  key <- E.decryptKMS $ T.encodeUtf8 $ T.pack getECTempCardEncryptedKey
-  setOption ECTempCardCred $ T.decodeUtf8 key
   case C.getEnv of
-    C.PROD  -> pure key
-    C.UAT   -> pure key
-    C.INTEG -> pure key
-    _       -> pure devKey
+    C.DEV -> pure devKey
+    _ -> do
+      mKey <- getOption ECTempCardCred
+      case mKey of
+        Just key -> pure $ B64.decodeLenient $ T.encodeUtf8 key
+        Nothing -> do
+          key <- E.decryptKMS $ B64.decodeLenient $ BC.pack getECTempCardEncryptedKey
+          setOption ECTempCardCred $ T.decodeUtf8 $ B64.encode key
+          pure key
   where
     devKey = fst $ B16.decode "bd3222130d110b9684dac9cc0903ce111b25e97ae93ddc2925f89c4ed6e41bab"
 
-
 ecMandateParamsCred :: Flow ByteString
 ecMandateParamsCred = do
-  key <- E.decryptKMS $ T.encodeUtf8 $ T.pack getECMandateParamsEncryptedKey
-  setOption ECMandateParamsCred $ T.decodeUtf8 key
   case C.getEnv of
-    C.PROD  -> pure key
-    C.UAT   -> pure key
-    C.INTEG -> pure key
-    _       -> pure devKey
+    C.DEV -> pure devKey
+    _ -> do
+      mbKey <- getOption ECMandateParamsCred
+      case mbKey of
+        Just key -> pure $ B64.decodeLenient $ T.encodeUtf8 key
+        Nothing -> do
+          key <- E.decryptKMS $ B64.decodeLenient $ BC.pack getECMandateParamsEncryptedKey
+          setOption ECMandateParamsCred $ T.decodeUtf8 $ B64.encode key
+          pure key
   where
     devKey = fst $ B16.decode "a231ccb856c125486f1891bc5646f30949efcd6d1c14b6acc439ef928b133c32"
 
 
 razorpayClientSecret :: String -> Flow ByteString
-razorpayClientSecret = E.decryptKMS . T.encodeUtf8 . T.pack
+razorpayClientSecret = E.decryptKMS . B64.decodeLenient . BC.pack
 
 getMySQLCfg :: Flow MySQLConfig
 getMySQLCfg = case C.getEnv of

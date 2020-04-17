d5cbdc5ab24e44a5b538c34d665e0157fe421d60
Ilia Rodionov <ilya.rodionov@gmail.com>
2020-04-17 18:04:48 +0300

    add --mode parameter for runEulerBackend.sh

--- runEulerBackend.sh
+++ runEulerBackend.sh
@@ -1,13 +1,54 @@
+#!/bin/bash
+
+# defaults
+MODE=NORMAL # could be REC, PLAYER, BULK
+
+for i in "$@"
+do
+case $i in
+    -m=REC|-m=PLAYER|-m=BULK|--mode=REC|--mode=PLAYER|--mode=BULK)
+    MODE="${i#*=}"
+    shift # past argument=value
+    ;;
+#    --default)
+#    DEFAULT=YES
+#    shift # past argument with no value
+#    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+echo "MODE            = ${MODE}"
+
+
+
+#echo "SEARCH PATH     = ${SEARCHPATH}"
+#echo "LIBRARY PATH    = ${LIBPATH}"
+#echo "DEFAULT         = ${DEFAULT}"
+#echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
+#if [[ -n $1 ]]; then
+#    echo "Last line of file specified as non-opt/last argument:"
+#    tail -1 $1
+#fi
+
 # RTS options
 REAL_CORES=$(lscpu --all --parse=CORE,SOCKET | grep -Ev "^#" | sort -u | wc -l)
-echo "Real cores = $REAL_CORES"
+echo "Real cores      = $REAL_CORES"
 
 RTS_OPTIONS="-A64M -AL256M -I0 -qb0 -qn$REAL_CORES -N$REAL_CORES"
 echo "RTS options = $RTS_OPTIONS"
 
-# Exports
+
+# Environment
 
 export NODE_ENV=development
+
+# ART mode
+if [[ "$MODE" = "REC" ]]; then
+  export RECORDER_ENABLED=TRUE
+  export RECORDER_RECORDINGS_DIR=/tmp/recsdir
+fi
 
 # Logger parameters
 export LOGGER_IS_ASYNC=False
@@ -44,3 +85,5 @@ export DEV_MYSQL_POOL_KEEP_ALIVE=10
 export DEV_MYSQL_POOL_RESOURCES_PER_STRIPE=50
 
 stack exec euler-backend --rts-options "$RTS_OPTIONS"
+
+EOF

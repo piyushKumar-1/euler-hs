7ebfd1a44c0ed5fb7d5ba0077c887971e282cd20
Ilia Rodionov <ilya.rodionov@gmail.com>
2020-05-21 14:06:03 +0300

    validation with typed errors in lieu of raw text WIP

--- lib/euler-hs/src/EulerHS/Extra/AltValidation.hs
+++ lib/euler-hs/src/EulerHS/Extra/AltValidation.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS -fno-warn-deprecations #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
-module EulerHS.Extra.Validation
+module EulerHS.Extra.AltValidation
   (
     -- * Extra Validation
     Transform(..)
@@ -33,9 +33,29 @@ import           GHC.TypeLits
 import           Type.Reflection
 
 
+data VErrorPayload = VErrorPayload
+  { status        :: Text
+  , status_id     :: Maybe Int
+  , error_code    :: Maybe Text
+  , error_message :: Maybe Text
+  , error_field   :: Maybe Text
+  }
+  deriving (Eq, Show, Generic)
+
+validationError :: VErrorPayload
+validationError = VErrorPayload
+  { status = "invalid_request_error"
+  , status_id = Nothing
+  , error_code = Just "INVALID_REQUEST"
+  , error_message = Nothing
+  , error_field = Nothing
+  }
+
+-- | Context contains the name of validated field
 type Ctx = Text
-type Errors = [Text]
-type V a = Validation [Text] a
+
+type Errors = [VErrorPayload]
+type V a = Validation [VErrorPayload] a
 
 -- TODO: Looks like Profunctor. Does it hold laws?
 -- | Represents Transformer from one type to another.
@@ -49,21 +69,43 @@ type Transformer a b = a -> ReaderT Ctx (Either Errors) b
 -- | Represents the value parameter validator.
 type Validator a = Transformer a a
 
--- | Takes error message and predicate and return validation function
+-- | Takes error message and predicate and returns validation function
+-- using default 'VErrorPayload'
 mkValidator :: Text -> (t -> Bool) -> Validator t
-mkValidator err pred v = ReaderT (\ctx -> if not $ pred v
-  then Left [ctx <> " " <> err]
+mkValidator msg pred v = ReaderT (\ctx -> if not $ pred v
+  then Left [validationError { error_message =  Just msg, error_field = Just ctx }]
   else pure v)
 
-guarded :: Text -> Bool -> ReaderT Ctx (Either Errors) ()
-guarded err pred | pred      = ReaderT (\_   -> pure ())
-                 | otherwise = ReaderT (\ctx -> Left [ctx <> " " <> err])
+-- | Takes error message and predicate and returns validation function
+-- using custom error
+mkCustomValidator :: VErrorPayload -> Text -> (t -> Bool) -> Validator t
+mkCustomValidator err msg pred v = ReaderT (\ctx -> if not $ pred v
+  then Left [err { error_message = Just msg, error_field = Just ctx }]
+  else pure v)
 
--- | Trying to decode Text to target type
+-- | Guards computations by a validation rule
+guarded :: Text -> Bool -> ReaderT Ctx (Either Errors) ()
+guarded msg pred | pred      = ReaderT (\_   -> pure ())
+                 | otherwise = ReaderT (\ctx -> Left [validationError {error_message = Just msg, error_field = Just ctx }])
+
+-- | Guards computations by a validation rule with a custom error
+guardedCustom :: VErrorPayload -> Bool -> ReaderT Ctx (Either Errors) ()
+guardedCustom err pred | pred      = ReaderT (\_   -> pure ())
+                 | otherwise = ReaderT (\ctx -> Left [err {error_field = Just ctx }])
+
+-- | Trying to decode 'Text' into a target type
 decode :: forall t . (Data t, Read t) => Transformer Text t
 decode v = ReaderT (\ctx -> case (readMaybe $ toString v) of
   Just x -> Right x
-  _      -> Left ["Can't decode " <> v <> " from field " <> ctx <> ", should be one of " <> showConstructors @t])
+  _      -> Left [ validationError { error_message = Just ("Can't decode value" <> v <> ", should be one of " <> showConstructors @t)
+                       , error_field = Just ctx}])
+
+-- | Trying to decode 'Text' into a target type, use custom error
+decodeCustom :: forall t . (Data t, Read t) => VErrorPayload -> Transformer Text t
+decodeCustom err v = ReaderT (\ctx -> case (readMaybe $ toString v) of
+  Just x -> Right x
+  _      -> Left [ err { error_message = Just ("Can't decode value" <> v <> ", should be one of " <> showConstructors @t)
+                       , error_field = Just ctx}])
 
 insideJust :: Transformer a b -> Transformer (Maybe a) (Maybe b)
 insideJust _ Nothing    = pure Nothing
@@ -72,7 +114,7 @@ insideJust val (Just a) = Just <$> val a
 -- | Trying to extract the argument from Maybe type
 --   if value is Nothing then raise Failure
 extractJust :: Transformer (Maybe a) a
-extractJust r = ReaderT (\ctx -> maybe (Left [ctx <> " not present"]) Right r)
+extractJust r = ReaderT (\ctx -> maybe (Left [validationError { error_message = Just "mandatory value is not present", error_field = Just ctx }]) Right r)
 
 extractMaybeWithDefault :: a -> Transformer (Maybe a) a
 extractMaybeWithDefault d r = ReaderT (\_ -> maybe (Right d) Right r)
@@ -84,13 +126,13 @@ withField
   => r -> Transformer v a -> Validation Errors a
 withField rec pav = fromEither $ runReaderT (pav $ getField @f rec) $ fieldName_ @f
 
--- | Run parser
+-- | Run a custom parser
 runParser
   :: forall a
    . ReaderT Ctx (Either Errors) a
   -> Text
   -> Validation Errors a
-runParser p msg = fromEither $ runReaderT p msg
+runParser p err = fromEither $ runReaderT p err
 
 -- | Return text representation of constructors of a given type
 showConstructors :: forall t . Data t => Text

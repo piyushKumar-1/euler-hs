9cfe488d9aca5ebbf084ae6e1100a1ad982a21cb
Koz Ross <koz.ross@retro-freedom.nz>
2020-12-02 10:21:55 +1300

    Fix euler build

--- build.nix
+++ build.nix
@@ -1,6 +1,15 @@
-{ devShell ? false }:
+{ devShell ? false, inCI ? false }:
 let
-  nix-inclusive = builtins.fetchTarball "https://github.com/juspay/nix-inclusive/archive/2ca1706029bfcf4bb7eaf17b4f32e49f436a148e.tar.gz";
+  flake-compat = fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/19576c2aea7f074ff0da818b21a8b0950ff6ec86.tar.gz";
+    sha256 = "13jf267qvd4fvph27flp5slrn6w2q26mhpakr8bj2ppqgyjamb79";
+  };
+
+  nix-inclusive = fetchTarball {
+    url = "https://github.com/juspay/nix-inclusive/archive/2ca1706029bfcf4bb7eaf17b4f32e49f436a148e.tar.gz";
+    sha256 = "1y3vhqnbh5kg906fpw22h670ppl8238xwv0dx7zdcp22212zdjnx";
+  };
+
   filter = import "${nix-inclusive}/inclusive.nix" { lib = (import <nixpkgs> {}).lib; };
   path =
     filter ./. [
@@ -12,5 +21,11 @@ let
       ./euler-hs.cabal
     ];
   attr = if devShell then "devShell" else "defaultPackage";
+  compat-attr = if devShell then "shellNix" else "defaultNix";
+
+  flake-drv =
+    if inCI
+    then (import flake-compat { src = path; }).${compat-attr}
+    else (builtins.getFlake (toString (builtins.unsafeDiscardStringContext path)));
 in
-(builtins.getFlake (toString (builtins.unsafeDiscardStringContext path))).${attr}.${builtins.currentSystem}
+flake-drv.${attr}.${builtins.currentSystem}

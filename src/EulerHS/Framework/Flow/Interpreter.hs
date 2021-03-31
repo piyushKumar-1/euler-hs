5eb064c8361ebebfb481a7a84b3a1de422b966e8
Aravind Gopal Mallapureddy <aravindgopal.ag@gmail.com>
2021-03-31 19:35:00 +0530

    set the response timeout from manager to request.

--- src/EulerHS/Framework/Flow/Interpreter.hs
+++ src/EulerHS/Framework/Flow/Interpreter.hs
@@ -33,6 +33,7 @@ import           EulerHS.Prelude
 import qualified Network.Connection as Conn
 import qualified Network.HTTP.Client as HTTP
 import qualified Network.HTTP.Client.TLS as TLS
+import           Network.HTTP.Client.Internal
 import qualified Network.HTTP.Types as HTTP
 import qualified Network.TLS as TLS
 import qualified Network.TLS.Extra.Cipher as TLS
@@ -192,7 +193,7 @@ interpretFlowMethod mbFlowGuid flowRt@R.FlowRuntime {..} (L.CallServantAPI mbMgr
           let S.ClientEnv manager baseUrl cookieJar makeClientRequest = S.mkClientEnv mngr bUrl
           let setR req = if HTTP.responseTimeout req == HTTP.responseTimeoutNone
                             then setRequestTimeout T.defaultTimeout req
-                            else req
+                            else req {HTTP.responseTimeout = mResponseTimeout mngr}
           eitherResult <- S.runClientM (T.runEulerClient (dbgLogger T.Debug) getLoggerMaskConfig bUrl clientAct) $
             S.ClientEnv manager baseUrl cookieJar (\url -> setR . makeClientRequest url)
           case eitherResult of

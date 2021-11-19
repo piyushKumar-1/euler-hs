3cb8e57a6ca609bfc2f0dc8ccce5e6dd7dca05fb
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2021-11-19 14:28:53 +0500

    Add withEulerDBR1

--- src/EulerHS/Extra/EulerDB.hs
+++ src/EulerHS/Extra/EulerDB.hs
@@ -6,6 +6,7 @@ module EulerHS.Extra.EulerDB (
   getEulerDbConf,
   getEulerDbConfR1,
   withEulerDB,
+  withEulerDBR1,
   ) where
 
 import           EulerHS.Language (MonadFlow, SqlDB, getOption,
@@ -38,7 +39,8 @@ getEulerDbConf = getEulerDbByConfig EulerDbCfg
 getEulerDbConfR1 :: (MonadFlow m, Exception e) => e -> m (DBConfig MySQLM)
 getEulerDbConfR1 = getEulerDbByConfig EulerDbCfgR1
 
-getEulerDbByConfig :: (MonadFlow m, Exception e, OptionEntity k v) => k -> e -> m v
+getEulerDbByConfig :: (MonadFlow m, Exception e, OptionEntity k (DBConfig MySQLM))
+  => k -> e -> m (DBConfig MySQLM)
 getEulerDbByConfig dbConf internalError = do
   dbcfg <- getOption dbConf
   case dbcfg of
@@ -47,9 +49,20 @@ getEulerDbByConfig dbConf internalError = do
       logErrorT "MissingDB identifier" "Can't find EulerDB identifier in options"
       throwException internalError
 
+-- NOTE: Does NOT run inside a transaction
 withEulerDB :: (MonadFlow m, Exception e) => e -> SqlDB MySQLM a -> m a
-withEulerDB internalError act = do
-  dbcfg <- getOption EulerDbCfg
+withEulerDB internalError act = withEulerDBGeneral EulerDbCfg internalError act
+
+-- | Runs the query against the MySQL read replica DB
+-- Falls back to using default MySQL DB if replica DB connection is not initialized
+-- NOTE: Does NOT run inside a transaction
+withEulerDBR1 :: (MonadFlow m, Exception e) => e -> SqlDB MySQLM a -> m a
+withEulerDBR1 internalError act = withEulerDBGeneral EulerDbCfg internalError act
+
+withEulerDBGeneral :: (MonadFlow m, Exception e, OptionEntity k (DBConfig MySQLM))
+  => k -> e -> SqlDB MySQLM a -> m a
+withEulerDBGeneral key internalError act = do
+  dbcfg <- getOption key
   case dbcfg of
     Just cfg -> withDB cfg act
     Nothing -> do

660e58f47f14d545de4eda9026495ea6135ec521
vaibhavsingh <vaibhav.singh@juspay.in>
2022-10-20 14:23:17 +0530

    EUL-3680 logging Fix

--- src/EulerHS/Framework/Interpreter.hs
+++ src/EulerHS/Framework/Interpreter.hs
@@ -271,6 +271,7 @@ interpretFlowMethod _ flowRt@R.FlowRuntime {..} (L.CallHTTP request manager next
       case eResponse of
         Left (err :: SomeException) -> do
           let errMsg = Text.pack $ displayException err
+          logJsonError errMsg (maskHTTPRequest getLoggerMaskConfig request)
           pure $ Left errMsg
         Right httpResponse -> do
           case translateHttpResponse httpResponse of

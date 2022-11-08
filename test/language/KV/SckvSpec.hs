fe971de3498c7ac01c3a3f2ef6ad1e16a663ca7b
Shubhanshu <shubhanshu.mani@juspay.in>
2022-11-08 17:09:52 +0530

    add a wrapper handler withServiceConfig that creates a SC entry and runs then test and after that deletes the entry

--- test/language/KV/SckvSpec.hs
+++ test/language/KV/SckvSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module KV.SckvSpec where
 
 import           EulerHS.Prelude
@@ -14,6 +15,8 @@ import qualified Data.Text as Text
 import EulerHS.KVConnector.Types hiding(kvRedis)
 import qualified Data.Aeson as A
 import Prelude (head)
+import qualified EulerHS.Types as T
+import           Database.Beam.MySQL (MySQLM)
 {-
 Things to test against insert
 1. The value should be present in the KV againt the right primary id KEY 
@@ -29,44 +32,57 @@ Things to test againt find
 -}
 -- KV_TESTae4b6\\\
 -- [\"]
+
+withServiceConfig :: (ServiceConfiguration -> T.DBConfig MySQLM -> L.Flow a) -> L.Flow a
+withServiceConfig act = do
+  dbConf <- getEulerDbConf
+  sc <- dummyServiceConfig
+  fmap fst $ generalBracket
+    (DB.createReturning dbConf meshConfig sc Nothing)
+    (\_ _ -> deleteServiceConfigValueFromKV sc)
+    (\eitherSC -> either (\err -> error $ show err) (\serviceConfig -> act serviceConfig dbConf) eitherSC)
+
 spec :: Spec
 spec = flowSpec $ do
-    itFlow "Should Add value to KV and fetch it successfully" $ do
-        dbConf <- getEulerDbConf
-        currentSC <- dummyServiceConfig
-        eitherVal <- DB.createReturning dbConf meshConfig currentSC Nothing
-        case eitherVal of
-          Left err -> error $ "Unable to insert : " <> (show err)
-          Right val -> do
-            let pKey = getLookupKeyByPKey val
-            let secKeys = getSecondaryLookupKeys val
-            L.logInfoT "SC" (decodeUtf8 $ A.encode val)
-            L.logInfoT "Pkey : Skey" $ pKey <> " : " <> (decodeUtf8 $ A.encode secKeys)
-            res <- join . hush <$> DB.findOne' dbConf meshConfig Nothing [Is name (Eq $ currentSC.name)]
-            asserting $ res `shouldBe` (Just val)
+    itFlow "Should fetch a created SC using secondary key" $ do
+        withServiceConfig $ (\serviceConfig dbConf -> do
+            eitherSC <- DB.findOne' dbConf meshConfig Nothing [Is name (Eq $ serviceConfig.name)]
+            when (isLeft eitherSC) $ error $ show eitherSC
+            asserting $ (join $ hush eitherSC) `shouldBe` (Just serviceConfig)
+          )
     itFlow "Should add primary key and secondary keys to redis on insert command" $ do
-        dbConf <- getEulerDbConf
-        currentSC <- dummyServiceConfig
-        eitherVal <- DB.createReturning dbConf meshConfig currentSC Nothing
-        case eitherVal of
-          Left err -> error $ "Unable to insert : " <> (show err)
-          Right val ->  do
-            let pKey = getLookupKeyByPKey val
-            let secKeys = getSecondaryLookupKeys val
-            valueFromPrimaryKey <- join . hush <$> (L.runKVDB kvRedis $ L.get $ encodeUtf8 pKey)
-            keysFromSecondaryKey <- hush <$> (L.runKVDB kvRedis $ L.lrange (encodeUtf8 $ head secKeys) 0 (-1))
-            valueViaSecondaryKey <- maybe (pure Nothing) (fmap (join . hush) . L.runKVDB kvRedis . L.get . head) keysFromSecondaryKey
-            asserting $ valueFromPrimaryKey `shouldBe` valueViaSecondaryKey
--- Assuming name column has unique constraint
-    itFlow "Should Reject duplicate entry to KV" $ do
-        dbConf <- getEulerDbConf
-        currentSC <- dummyServiceConfig
-        eitherVal <- DB.createReturning dbConf meshConfig currentSC Nothing
-        case eitherVal of
-          Left err -> error $ "Unable to insert : " <> (show err)
-          Right _ -> do 
-            eitherDuplicate <- DB.createReturning dbConf meshConfig currentSC Nothing
-            asserting $ isLeft eitherDuplicate `shouldBe` True
+        withServiceConfig $ (\serviceConfig _dbConf -> do
+            let pKey = getLookupKeyByPKey serviceConfig
+            let secKeys = getSecondaryLookupKeys serviceConfig
+            (valueFromPrimaryKey :: Maybe ServiceConfiguration) <- getValueFromPrimaryKey pKey
+            valueFromSecondaryKeys<- (snd . head) <$> getValueFromSecondaryKeys secKeys
+            asserting $ valueFromPrimaryKey `shouldBe` valueFromSecondaryKeys
+          )
+
+
+getValueFromPrimaryKey :: FromJSON a => Text -> L.Flow (Maybe a)
+getValueFromPrimaryKey pKey = do
+  res <- L.runKVDB kvRedis $ L.get $ encodeUtf8 pKey
+  case res of
+    Right (Just val) -> either (error . show) (pure . Just) (A.eitherDecodeStrict val)
+    Right Nothing -> L.logInfoT "KEY_NOT_FOUND" pKey $> Nothing
+    Left err -> error $ show err
+
+getValueFromSecondaryKeys :: FromJSON a => [Text] -> L.Flow [(Text,a)]
+getValueFromSecondaryKeys secKeys = do
+  eitherRefKeys <- L.runKVDB kvRedis $ L.lrange (encodeUtf8 $ head secKeys) 0 (-1)
+  case eitherRefKeys of
+    Right refKeys -> fmap catMaybes $ sequence $ go <$> refKeys
+    Left err -> error $ show err
+  where
+    go :: FromJSON a => ByteString -> L.Flow (Maybe (Text,a))
+    go bkey = do
+      let key = decodeUtf8 bkey
+      mbRes <- getValueFromPrimaryKey key
+      pure $ case mbRes of
+                Just res -> Just (key,res)
+                Nothing -> Nothing
+
 
 dummyServiceConfig :: L.Flow ServiceConfiguration
 dummyServiceConfig = do
@@ -77,3 +93,11 @@ dummyServiceConfig = do
     , name = "KV_TEST" <> randomName
     , value = Just "VALUE"
     }
+
+
+deleteServiceConfigValueFromKV :: ServiceConfiguration -> L.Flow ()
+deleteServiceConfigValueFromKV sc = do
+  let pKey = getLookupKeyByPKey sc
+  let secKeys = getSecondaryLookupKeys sc
+  void $ hush <$> (L.runKVDB kvRedis $ L.del ([encodeUtf8 pKey]))
+  void $ hush <$> (L.runKVDB kvRedis $ L.del (encodeUtf8 <$> secKeys))

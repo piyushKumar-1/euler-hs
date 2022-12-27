cc85acca309a1f11dfbfd21d46fcdde27b1e3912
Harshit <harshit.parashar@juspay.in>
2022-12-27 20:25:47 +0530

    EUL-5492 added metric logging changes

--- src/EulerHS/KVConnector/Metrics.hs
+++ src/EulerHS/KVConnector/Metrics.hs
@@ -9,17 +9,35 @@ import qualified EulerHS.Language as L
 import           EulerHS.Options  (OptionEntity)
 import           Euler.Events.MetricApi.MetricApi
 import qualified Juspay.Extra.Config as Conf
+import           EulerHS.KVConnector.Types  (DBLogEntry(..))
 
-incrementKVMetric :: L.MonadFlow m => KVMetricHandler -> KVMetric -> Text -> Text -> Source -> m ()
-incrementKVMetric handle metric tag action source = do
-  L.runIO $ ((kvCounter handle) (metric, tag, action, source))
+incrementKVMetric :: L.MonadFlow m => KVMetricHandler -> KVMetric -> DBLogEntry a -> m ()
+incrementKVMetric handle metric dblog = do
+  let mid = fromMaybe "" $ _merchant_id dblog
+  let tag = fromMaybe "" $ _apiTag dblog
+  let source = show $ _source dblog
+  let model = _model dblog
+  let action = _action dblog
+  L.runIO $ ((kvCounter handle) (metric, tag, action, source,model,mid))
 
 data KVMetricHandler = KVMetricHandler
-  { kvCounter :: (KVMetric, Text, Text, Source) -> IO ()
+  { kvCounter :: (KVMetric, Text, Text, Text,Text,Text) -> IO ()
   }
 
 data KVMetric = KVAction
 
+
+-- data DBLogEntry a = DBLogEntry
+--   { _log_type     :: Text
+--   , _action       :: Text
+--   , _data         :: a
+--   , _latency      :: Int
+--   , _model        :: Text
+--   , _cpuLatency   :: Integer
+--   , _source       :: Text
+--   , _apiTag       :: Maybe Text
+--   , _merchant_id  :: Maybe Text
+--   }
 data Source = KV | SQL | KV_AND_SQL
     deriving Show
 
@@ -27,13 +45,15 @@ mkKVMetricHandler :: IO KVMetricHandler
 mkKVMetricHandler = do
   metrics <- register collectionLock
   pure $ KVMetricHandler $ \case
-    (KVAction, tag, action, source)   ->
-      inc (metrics </> #kv_action) tag action source
+    (KVAction, tag, action, source, model , mid)   ->
+      inc (metrics </> #kv_action) tag action source model mid
 
 kv_action = counter #kv_action
       .& lbl @"tag" @Text
       .& lbl @"action" @Text
-      .& lbl @"source" @Source
+      .& lbl @"source" @Text
+      .& lbl @"model" @Text
+      .& lbl @"mid" @Text
       .& build
 
 collectionLock =
@@ -56,9 +76,9 @@ isKVMetricEnabled = fromMaybe False $ readMaybe =<< Conf.lookupEnvT "KV_METRIC_E
 
 ---------------------------------------------------------
 
-incrementMetric :: (HasCallStack, L.MonadFlow m) => KVMetric -> Text -> Text -> Source -> m ()
-incrementMetric metric tag action source = do
+incrementMetric :: (HasCallStack, L.MonadFlow m) => KVMetric -> DBLogEntry a ->  m ()
+incrementMetric metric dblog = do
   env <- L.getOption KVMetricCfg
   case env of
-    Just val -> incrementKVMetric val metric tag action source
+    Just val -> incrementKVMetric val metric dblog
     Nothing -> pure ()
--- src/EulerHS/KVConnector/Types.hs
+++ src/EulerHS/KVConnector/Types.hs
@@ -25,7 +25,7 @@ import qualified Database.Beam.Backend.SQL as B
 import           Database.Beam.Schema (FieldModification, TableField)
 import           Sequelize (Column, Set)
 import qualified EulerHS.Types as T
-
+import           Data.Aeson ((.=))
 
 ------------ TYPES AND CLASSES ------------
 
@@ -130,6 +130,11 @@ data KVCEnabledTables = KVCEnabledTables
 
 instance T.OptionEntity KVCEnabledTables FeatureConfig
 
+data MerchantID = MerchantID 
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (ToJSON, FromJSON)
+instance T.OptionEntity MerchantID Text
+
 data FeatureConfig = FeatureConfig -- TODO: Move this from here to euler-db
   { enableAll        :: Bool
   , disableAny       :: Maybe [Text]
@@ -143,3 +148,28 @@ data RolloutConfig = RolloutConfig
   , rollout    :: Int
   }
   deriving (Show, Eq, Ord, Generic, ToJSON, FromJSON)
+
+
+data DBLogEntry a = DBLogEntry
+  { _log_type     :: Text
+  , _action       :: Text
+  , _data         :: a
+  , _latency      :: Int
+  , _model        :: Text
+  , _cpuLatency   :: Integer
+  , _source       :: Text
+  , _apiTag       :: Maybe Text
+  , _merchant_id  :: Maybe Text
+  }
+  deriving stock (Generic)
+  -- deriving anyclass (ToJSON)
+instance (ToJSON a) => ToJSON (DBLogEntry a) where
+  toJSON val = A.object [ "log_type" .= _log_type val
+                        , "action" .= _action val
+                        , "latency" .= _latency val
+                        , "model" .= _model val
+                        , "cpuLatency" .= _cpuLatency val
+                        , "data" .= _data val
+                        , "source" .= _source val
+                        , "api_tag" .= _apiTag val
+                      ]

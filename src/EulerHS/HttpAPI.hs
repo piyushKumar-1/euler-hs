e283c4bff69542f0dfc8e8273f5a2d4e71948c2e
eswar2001 <eswar.tadiparth@juspay.in>
2023-02-13 20:03:44 +0530

    maskRequestURL fix

--- src/EulerHS/HttpAPI.hs
+++ src/EulerHS/HttpAPI.hs
@@ -495,7 +495,7 @@ maskHTTPRequest mbMaskConfig request = HTTPRequestMasked
 
     getMaskText = maybe defaultMaskText (fromMaybe defaultMaskText . Log._maskText) mbMaskConfig
 
-    maskRequestURL = Text.takeWhile (== ('?')) request.getRequestURL
+    maskRequestURL = Text.takeWhile (/= ('?')) request.getRequestURL
 
     maskedRequestBody =
           parseRequestResponseBody (shouldMaskKey mbMaskConfig) getMaskText (getContentTypeForHTTP requestHeaders)
